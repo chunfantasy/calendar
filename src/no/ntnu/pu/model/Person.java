5e2e83da02e606a30e76b8268a2ad02d829e1d51
Lima92 <anders92lima@yahoo.no>
2014-03-10 14:44:01 +0100

    Added constructor, getters and setters

--- src/no/ntnu/pu/model/Person.java
+++ src/no/ntnu/pu/model/Person.java
@@ -1,8 +1,35 @@
 package no.ntnu.pu.model;
 
-public class Person{
+public class Person implements Participant{
 	private String name;
 	private String email;
 	private String[] phoneNumbers;
-	
+
+    public Person(String Name){
+        name = Name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumbers(String[] phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String[] getPhoneNumbers() {
+        return phoneNumbers;
+    }
 }
