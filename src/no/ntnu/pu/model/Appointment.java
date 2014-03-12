415732efd0d69f4eb18a40e7f30bdda30fda4209
chunfantasy <chunfantasy@hotmail.com>
2014-03-12 14:39:15 +0100

    finish insert

--- src/no/ntnu/pu/model/Appointment.java
+++ src/no/ntnu/pu/model/Appointment.java
@@ -1,56 +1,69 @@
 package no.ntnu.pu.model;
 
-import java.sql.Time;
+import java.util.Date;
 import java.util.ArrayList;
 
 public class Appointment {
 	private String title;
-	private Time startTime;
-	private Time endTime;
+	private Date startTime;
+	private Date endTime;
 	private String adress;
 	private Room meetingRoom;
 	private String description;
 	private ArrayList<Participant> participants;
-	
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public Time getStartTime() {
+
+	public Date getStartTime() {
 		return startTime;
 	}
-	public void setStartTime(Time startTime) {
+
+	public void setStartTime(Date startTime) {
 		this.startTime = startTime;
 	}
-	public Time getEndTime() {
+
+	public Date getEndTime() {
 		return endTime;
 	}
-	public void setEndTime(Time endTime) {
+
+	public void setEndTime(Date endTime) {
 		this.endTime = endTime;
 	}
+
 	public String getAdress() {
 		return adress;
 	}
+
 	public void setAdress(String adress) {
 		this.adress = adress;
 	}
+
 	public Room getMeetingRoom() {
 		return meetingRoom;
 	}
+
 	public void setMeetingRoom(Room meetingRoom) {
 		this.meetingRoom = meetingRoom;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public ArrayList<Participant> getParticipants() {
 		return participants;
 	}
+
 	public void setParticipants(ArrayList<Participant> participants) {
 		this.participants = participants;
 	}
--- src/no/ntnu/pu/storage/ServerStorage.java
+++ src/no/ntnu/pu/storage/ServerStorage.java
@@ -2,13 +2,17 @@ package no.ntnu.pu.storage;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Date;
 
-import java.sql.PreparedStatement;
-
+import no.ntnu.pu.model.Appointment;
+import no.ntnu.pu.model.Group;
 import no.ntnu.pu.model.Person;
+import no.ntnu.pu.model.Room;
 
 public class ServerStorage extends Storage {
 	private Connection con;
@@ -17,6 +21,7 @@ public class ServerStorage extends Storage {
 	private ResultSet rs;
 	private String sql;
 
+	// Connect to the database
 	public void connect() throws SQLException {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -25,24 +30,111 @@ public class ServerStorage extends Storage {
 			e.printStackTrace();
 		}
 
-		con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test",
-				"testUser", "test");
+		con = DriverManager.getConnection(
+				"jdbc:mysql://127.0.0.1:3306/calendar", "root", "123");
 		con.setAutoCommit(false);
 	}
 
-	public boolean create(Object o) throws SQLException {
+	// initiate database
+	public void initiate() throws SQLException {
+
+		stmt = con.createStatement();
+
+		//@formatter:off
+		// create table person
+		sql = "DROP TABLE IF EXISTS person";
+		stmt.execute(sql);
+		sql = "DROP TABLE IF EXISTS appointment";
+		stmt.execute(sql);
+		sql = "DROP TABLE IF EXISTS meetingroom";
+		stmt.execute(sql);
+		sql = "DROP TABLE IF EXISTS meetinggroup";
+		stmt.execute(sql);
+
+		sql = "CREATE TABLE person (" 
+				+ "id int auto_increment primary key, "
+				+ "email varchar(20), " 
+				+ "name varchar(10), "
+				+ "title varchar(10))";
+		stmt.execute(sql);
+
+		// create talbe meetingroom
+		sql = "CREATE TABLE meetingroom ("
+				+ "id int auto_increment primary key, "
+				+ "roomname varchar(15))";
+		stmt.execute(sql);
+
+		sql = "CREATE TABLE appointment ("
+				+ "id int auto_increment primary key, " 
+				+ "title varchar(20), "
+				+ "starttime datetime, " 
+				+ "endtime datetime, "
+				+ "adress varchar(30), " 
+				+ "meetingroomid int, "
+				+ "foreign key(meetingroomid) references meetingroom(id), "
+				+ "description varchar(50))";
+		stmt.execute(sql);
+
+		sql = "CREATE TABLE meetinggroup (" 
+				+ "id int auto_increment primary key, "
+				+ "name varchar(15)," 
+				+ "email varchar(20))";
+		stmt.execute(sql);
+		
+		//@formatter:on
+		con.commit();
+	}
+
+	// Save an object in the database
+	public boolean insert(Object o) throws SQLException {
+		// table person
 		if (o instanceof Person) {
-			sql = "INSERT INTO person VALUES(?, ?)";
+			Person p = (Person) o;
+			sql = "INSERT INTO person(email, name, title) VALUES(?, ?, ?)";
 			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, ((Person) o).getEmail());
-			pstmt.setString(2, ((Person) o).getName());
-			
+			pstmt.setString(1, p.getEmail());
+			pstmt.setString(2, p.getName());
+			pstmt.setString(3, p.getTitle());
+			pstmt.executeUpdate();
+		}
+
+		// talbe meetingroom
+		if (o instanceof Room) {
+			Room r = (Room) o;
+			sql = "INSERT INTO meetingroom(roomname) VALUES(?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, r.getRoomname());
+			pstmt.executeUpdate();
+		}
+
+		// table appointment appointment_participant
+		else if (o instanceof Appointment) {
+			Appointment a = (Appointment) o;
+			sql = "INSERT INTO appointment(title, starttime, endtime, adress, description, meetingroomid) "
+					+ "VALUES(?, ?, ?, ?, ?, ?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, a.getTitle());
+			pstmt.setTimestamp(2, new Timestamp(a.getStartTime().getTime()));
+			pstmt.setTimestamp(3, new Timestamp(a.getEndTime().getTime()));
+			pstmt.setString(4, a.getAdress());
+			pstmt.setString(5, a.getDescription());
+			pstmt.setInt(6, a.getMeetingRoom().getId());
+			pstmt.executeUpdate();
+
+		}
+
+		else if (o instanceof Group) {
+			Group g = (Group) o;
+			sql = "INSERT INTO meetinggroup(email, name) VALUES(?, ?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, g.getEmail());
+			pstmt.setString(2, g.getName());
+			pstmt.executeUpdate();
+
 		} else
 			return false;
 		
-		pstmt.executeUpdate();
 		con.commit();
-		con.close();
 		return true;
 	}
 
@@ -50,11 +142,27 @@ public class ServerStorage extends Storage {
 		// TODO Auto-generated method stub
 		ServerStorage serverStorage = new ServerStorage();
 		serverStorage.connect();
-		Person p = new Person();
-		p.setEmail("email");
-		p.setName("name");
-		serverStorage.create(p);
+		serverStorage.initiate();
 
+		Person p = new Person("a");
+		p.setEmail("email");
+		p.setTitle("title");
+
+		serverStorage.insert(p);
+
+		Room r = new Room("P15");
+		r.setId(1);
+		serverStorage.insert(r);
+
+		Appointment a = new Appointment();
+		a.setTitle("gogogo");
+		a.setStartTime(new Date());
+		a.setEndTime(new Date());
+		a.setMeetingRoom(r);
+		serverStorage.insert(a);
+
+		Group g = new Group("super group 12");
+		serverStorage.insert(g);
 	}
 
 }
