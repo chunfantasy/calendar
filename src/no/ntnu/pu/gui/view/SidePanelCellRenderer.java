eb3b42fdc4b0ab98ae6282a6531d6e82e9ccd945
Lima92 <anders92lima@yahoo.no>
2014-03-14 14:35:03 +0100

    Updated the CellRenderer with colors and text. #23

--- src/no/ntnu/pu/gui/view/SidePanelCellRenderer.java
+++ src/no/ntnu/pu/gui/view/SidePanelCellRenderer.java
@@ -1,13 +1,10 @@
 package no.ntnu.pu.gui.view;
 
 import javafx.scene.control.ListCell;
-import no.ntnu.pu.model.Alarm;
+import no.ntnu.pu.model.*;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.border.TitledBorder;
+import javax.swing.border.*;
 import java.awt.*;
 
 /**
@@ -15,25 +12,61 @@ import java.awt.*;
  */
 public class SidePanelCellRenderer extends JLabel implements ListCellRenderer {
 
-    protected  DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
+    private static final Color ALARM_COLOR = new Color(205, 0, 0);
+    private static final Color INVITATION_COLOR = new Color(65, 121, 205);
+    private static final Color CHANGENOTI_COLOR= new Color(205, 201, 0);
+    private static final Color DECLINENOTI_COLOR = new Color(205, 134, 0);
+    private static final Color APPOINTMENT_COLOR = new Color(0, 205, 0);
+
 
     @Override
     public Component getListCellRendererComponent(JList list, Object value,
                                                   int index, boolean isSelected,
                                                   boolean cellHasFocus) {
 
-        if(value instanceof String){
-            String s = (String) value;
-            setText(s);
-            if(isSelected){
-                setBackground(Color.BLUE);
-                setForeground(Color.CYAN);
+        if(value instanceof Invitation){
+            Invitation invi = (Invitation) value;
+            setText("Invitasjon til: " + invi.getAppointment().getTitle());
+            setCellBorder(INVITATION_COLOR,cellHasFocus);
+
+        }else if(value instanceof Alarm){
+            Alarm a = (Alarm) value;
+            setText(a.getAppointment().getTitle()+" "+a.getTime());
+            if(cellHasFocus){
+                this.setBorder(BorderFactory.createMatteBorder(
+                        4, 5, 4, 4, ALARM_COLOR));
             }else{
-                setForeground(list.getForeground());
-                setBackground(list.getBackground());
+                this.setBorder(BorderFactory.createMatteBorder(
+                        3, 5, 3, 3, ALARM_COLOR));
             }
-            setOpaque(true);
+
+        }else if(value instanceof ChangeNotification){
+            ChangeNotification cn = (ChangeNotification) value;
+            setText(String.valueOf(cn.getChangedProperties().length)+" felt er endret i "+ cn.getAppointment().getTitle());
+            setCellBorder(CHANGENOTI_COLOR,cellHasFocus);
+
+        }else if(value instanceof DeclineNotification){
+            DeclineNotification dn = (DeclineNotification) value;
+            setText(dn.getDecliner().getName()+ " har avslått invitasjonen til "+dn.getAppointment().getTitle());
+            setCellBorder(DECLINENOTI_COLOR,cellHasFocus);
+
+        }else if(value instanceof Appointment){
+            Appointment app = (Appointment) value;
+            setText(app.getTitle()+ " klokken "+app.getStartTime());
+            setCellBorder(APPOINTMENT_COLOR,cellHasFocus);
         }
+
+        setOpaque(true);
         return this;
+    }
+
+    private void setCellBorder(Color c, Boolean focus){
+        if(focus){
+            this.setBorder(BorderFactory.createMatteBorder(
+                1, 5, 1, 1, c));
+        }else{
+            this.setBorder(BorderFactory.createMatteBorder(
+                    0, 5, 0, 0, c));
+        }
     }
 }
