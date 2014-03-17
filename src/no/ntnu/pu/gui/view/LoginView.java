d0d2fdb8b16d4beddd6f1040ef197c651e1d3c45
Håkon <haakon.t@gmail.com>
2014-03-17 13:07:09 +0100

    #12

--- src/no/ntnu/pu/gui/view/LoginView.java
+++ src/no/ntnu/pu/gui/view/LoginView.java
@@ -29,23 +29,23 @@ public class LoginView extends JPanel {
         panel.setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
 
-        //Labels
+        /**Labels**/
         lblUsername = new JLabel("Brukernavn: ", JLabel.LEFT);
         lblPassword = new JLabel("Passord: ", JLabel.LEFT);
 
-        //Textfields (with listeners)
+        /**Textfields (with listeners)**/
         userField = new JTextField(20);
         userField.addActionListener(new myUserAction());
         passField = new JPasswordField(20);
         passField.addActionListener(new myPasswordAction());
 
-        //Buttons (with listeners)
+        /**Buttons (with listeners)**/
         btnLogin = new JButton("Logg inn");
         btnLogin.addActionListener(new myLoginAction());
         btnForgottenPassword = new JButton("Glemt passord?");
         btnForgottenPassword.addActionListener(new myForgottenAction());
 
-        //Adding components to panel
+        /**Adding components to panel**/
         panelAdd(1, 0, 0, constraints, lblUsername, panel);
         panelAdd(1, 0, 1, constraints, lblPassword, panel);
         panelAdd(2, 1, 0, constraints, userField, panel);
@@ -110,6 +110,7 @@ public class LoginView extends JPanel {
         frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pane.add(new LoginView());
         frmMain.pack();
+        frmMain.setLocationRelativeTo(null);
         frmMain.setResizable(false);
         frmMain.setVisible(true);
     }
