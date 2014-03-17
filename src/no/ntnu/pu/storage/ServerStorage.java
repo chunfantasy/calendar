c7599415fc9b63c0e531d82afc3b057bf1947022
chunfantasy <chunfantasy@hotmail.com>
2014-03-17 13:10:50 +0100

    merge master from storage, delete the third line in SidePanelCellRenderer

--- src/no/ntnu/pu/storage/ServerStorage.java
+++ src/no/ntnu/pu/storage/ServerStorage.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
@@ -267,7 +268,8 @@ public class ServerStorage implements Storage {
 		a.setParticipants(participants);
 		serverStorage.insert(a);
 
-		System.out.println(serverStorage.deletePersonByEmail("email"));
+		System.out.println();
+		
 
 	}
 
