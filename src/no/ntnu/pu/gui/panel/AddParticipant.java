7bd29db3a78945087daca7c1b93a450471b3f815
Petter <bakkan@live.no>
2014-03-17 13:15:49 +0100

    --

--- src/no/ntnu/pu/gui/panel/AddParticipant.java
+++ src/no/ntnu/pu/gui/panel/AddParticipant.java
@@ -1,6 +1,7 @@
 package no.ntnu.pu.gui.panel;
 
 import no.ntnu.pu.model.Participant;
+import no.ntnu.pu.model.Person;
 
 import java.awt.Component;
 import java.awt.GridBagConstraints;
@@ -8,26 +9,34 @@ import java.awt.GridBagLayout;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.BevelBorder;
+import javax.swing.border.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 
-public class AddParticipant extends JPanel implements ActionListener, ListSelectionListener {
+public class AddParticipant extends JPanel implements ActionListener, ListSelectionListener, FocusListener {
     private JButton addButton, cancelButton;
     private JTextField searchField;
-    private JList<Participant> participantList;
+    private JLabel searchLabel;
+    private JTable participantTable;
     private JToggleButton personButton, groupButton;
     private ButtonGroup buttonGroup;
-    private DefaultListModel<Participant> participantListModel;
+    private DefaultTableModel personTableModel, groupTableModel;
     private JPanel totalGUI;
+    private Object[][] Personer = {{"Bernt"}, {"Gerd"}, {"Gjørdis"}, {"Olga"}, {"Per Hege"}, {"Arnold"}, {"Ludvig"}}, Grupper = {{"Juventus"}, {"Inter"}, {"Napoli"}, {"Milan"}, {"Roma"}};
+    private String[] HEADER = {"Navn"};
 
     public JPanel createContentPane(){
         totalGUI = new JPanel();
+
 
         // Gridbag
         GridBagConstraints gbc;
@@ -41,46 +50,67 @@ public class AddParticipant extends JPanel implements ActionListener, ListSelect
         catch (IllegalAccessException e) {}
         catch (UnsupportedLookAndFeelException e) {}
 
-        // participantListModel
-        participantListModel = new DefaultListModel<Participant>();
+        // Icons
+        ImageIcon icon = new ImageIcon(getClass().getResource("search.png"));
 
+        // personTableModel
+        personTableModel = new DefaultTableModel(Personer, HEADER){
+            public boolean isCellEditable(int row, int column){
+                return false;
+            }
+        };
+
+        // groupTableModel
+        groupTableModel = new DefaultTableModel(Grupper, HEADER){
+            public boolean isCellEditable(int row, int column){
+                return false;
+            }
+        };
+
+        // Buttons
         addButton = new JButton("Legg til");
         addButton.addActionListener(this);
+        addButton.setFocusable(false);
 
         cancelButton = new JButton("Avbryt");
         cancelButton.addActionListener(this);
-
-        /**
-         searchField with Image
-         final ImageIcon image = new ImageIcon(getClass().getResource("search.png"));
-         searchField = new JTextField("Search"){
-         protected void paintComponent(Graphics g){
-         super.paintComponent(g);
-         int centerPoint = (getHeight() - image.getIconHeight())/2;
-         g.drawImage(image, 0, centerPoint, this);
-
-         }
-         };
-
-         searchField.setMargin(new Insets(0, image.getWidth(), 0, 0));
-         **/
+        cancelButton.setFocusable(false);
 
         searchField = new JTextField("Søk");
         searchField.setColumns(15);
+        searchField.addFocusListener(this);
+        searchField.addActionListener(this);
+        searchField.setHorizontalAlignment(SwingConstants.LEFT);
+        searchLabel = new JLabel(icon);
+        searchLabel.setPreferredSize(new Dimension(13, 13));
+        searchLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 
-        participantList = new JList<Participant>(participantListModel);
-        participantList.setModel(participantListModel);
-        participantList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        participantList.addListSelectionListener(this);
-        JScrollPane scroll = new JScrollPane(participantList);
-        JLabel label = new JLabel("Navn", JLabel.LEFT);
-        label.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.DARK_GRAY, Color.DARK_GRAY));
-        scroll.setColumnHeaderView(label);
+        // participantTable
+        participantTable = new JTable();
+        // setup
+        participantTable.setAutoCreateRowSorter(true);
+        participantTable.setModel(personTableModel);
+        participantTable.setShowGrid(false);
+        // Selection
+        participantTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        participantTable.setCellSelectionEnabled(false);
+        participantTable.setColumnSelectionAllowed(false);
+        participantTable.setRowSelectionAllowed(true);
+        // Renderer
+        participantTable.setDefaultRenderer(participantTable.getColumnClass(0), new MyTableCellRenderer());
+        participantTable.getColumnModel().getColumn(0).setHeaderRenderer(new MyTableHeaderRenderer());
+        // Listener
+        participantTable.addFocusListener(this);
+        // ScrollPane
+        JScrollPane scroll = new JScrollPane(participantTable);
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scroll.setPreferredSize(new Dimension(180, 150));
+        participantTable.setFillsViewportHeight(true);
 
+        // buttons
         personButton = new JToggleButton("Person");
         personButton.addActionListener(this);
+        personButton.setSelected(true);
 
         groupButton = new JToggleButton("Gruppe");
         groupButton.addActionListener(this);
@@ -90,7 +120,8 @@ public class AddParticipant extends JPanel implements ActionListener, ListSelect
         buttonGroup.add(groupButton);
 
 
-        setupGBC(2, 1, 0.5, 0, 1, gbc, searchField, true);
+        setupGBC(1, 1, 1, 0, 1, gbc, searchLabel, true);
+        setupGBC(2, 1, 1, 1, 1, gbc, searchField, true);
         setupGBC(4, 4, 0.5, 0, 2, gbc, scroll, true);
         setupGBC(1, 1, 0.5, 0, 6, gbc, addButton, false);
         setupGBC(1, 1, 0.5, 3, 6, gbc, cancelButton, false);
@@ -103,13 +134,13 @@ public class AddParticipant extends JPanel implements ActionListener, ListSelect
 
     }
 
-    private static void createAndShowGUI(){
+    public static void createAndShowGUI(){
         JFrame frame = new JFrame("Legg til deltaker");
         JFrame.setDefaultLookAndFeelDecorated(true);
         AddParticipant view = new AddParticipant();
         frame.setContentPane(view.createContentPane());
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
         frame.pack();
     }
@@ -131,13 +162,90 @@ public class AddParticipant extends JPanel implements ActionListener, ListSelect
     }
 
     @Override
-    public void actionPerformed(ActionEvent arg0) {
+    public void actionPerformed(ActionEvent e) {
+        // Person - Group toggle
+        if(e.getSource() == personButton){
+            participantTable.setModel(personTableModel);
+            searchField.setText("Søk");
+        }
+        if (e.getSource() == groupButton){
+            participantTable.setModel(groupTableModel);
+            searchField.setText("Søk");
+        }
+        // searchField - search for specific word
+        if (e.getSource() == searchField){
+            DefaultTableModel searchList = null;
+            if (personButton.isSelected()){
+                searchList = personTableModel;
+            } else {
+                searchList = groupTableModel;
+            }
+            String text = searchField.getText();
+            for (int row = 0; row<participantTable.getRowCount(); row++){
+                if (text.equals(searchList.getValueAt(row, 0))){
+                    // this will automatically set the view of the scroll in the location of the value
+                    participantTable.scrollRectToVisible(participantTable.getCellRect(row, 0, true));
+
+                    // this will automatically set the focus of the searched/selected row/value
+                    participantTable.setRowSelectionInterval(row, row);
+                }
+            }
+        }
+
 
     }
 
     @Override
     public void valueChanged(ListSelectionEvent arg0) {
 
+    }
+
+    @Override
+    public void focusGained(FocusEvent e) {
+        if (e.getSource() == searchField){
+            searchField.setText("");
+        }
+
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+        if (e.getSource() == searchField){
+            searchField.setText("Søk");
+        }
+        if (e.getSource() == participantTable){
+            participantTable.clearSelection();
+        }
+    }
+
+    class MyTableCellRenderer extends DefaultTableCellRenderer {
+
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+//            Component c = super.getTableCellRenderer
+            Border outside = new MatteBorder(2, 0, 2, 0, new Color(0, 0, 0));
+            Border inside = new EmptyBorder(0, 1, 0, 1);
+            Border highLight = new CompoundBorder(outside, inside);
+            if (!isSelected){
+                setBackground(row % 2 == 0 ? Color.WHITE : new Color(130, 169, 215));
+            }
+            if (table.isRowSelected(row)){
+                setBorder(highLight);
+            }
+            table.getTableHeader().setReorderingAllowed(false);
+            return this;
+        }
+    }
+    class MyTableHeaderRenderer extends DefaultTableCellRenderer {
+
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+            setToolTipText((String) value);
+            setBackground(Color.LIGHT_GRAY);
+            setHorizontalAlignment(JLabel.CENTER);
+            setBorder(new MatteBorder(0, 0, 2, 1, Color.black));
+            return this;
+        }
     }
 
     public static void main(String[] args){
@@ -147,5 +255,4 @@ public class AddParticipant extends JPanel implements ActionListener, ListSelect
             }
         });
     }
-
 }
