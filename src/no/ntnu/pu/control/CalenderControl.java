9fb21b84c6efa06d8d34285712ff8c6e5938f702
Nils Herde <mail@hernil.com>
2014-03-18 13:32:43 +0100

    Made methods static

--- src/no/ntnu/pu/control/CalenderControl.java
+++ src/no/ntnu/pu/control/CalenderControl.java
@@ -4,19 +4,19 @@ import no.ntnu.pu.model.Appointment;
 import no.ntnu.pu.storage.ServerStorage;
 
 public class CalenderControl {
-	ServerStorage storage;
+	static ServerStorage storage;
 	
 	public void CalendarControl() {
 		storage = new ServerStorage();
 	}
 	
-	public void addAppointment(Appointment appointment) {
+	public static void addAppointment(Appointment appointment) {
 		storage.insertAppointment(appointment);
 	}
-	public void deleteAppointment(Appointment appointment) {
+	public static void deleteAppointment(Appointment appointment) {
 		storage.deleteAppointmentById(appointment.getId());
 	}
-	public void updateAppointment(Appointment appointment) {
+	public static void updateAppointment(Appointment appointment) {
 		storage.updateAppointment(appointment);
 	}
 
