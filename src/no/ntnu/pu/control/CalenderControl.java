5b389a15ffe969030604432fb60285c22d640cf0
Nils Herde <mail@hernil.com>
2014-03-18 11:56:46 +0100

    Added methods to CalendarControl

--- src/no/ntnu/pu/control/CalenderControl.java
+++ src/no/ntnu/pu/control/CalenderControl.java
@@ -1,5 +1,23 @@
 package no.ntnu.pu.control;
 
+import no.ntnu.pu.model.Appointment;
+import no.ntnu.pu.storage.ServerStorage;
+
 public class CalenderControl {
+	ServerStorage storage;
+	
+	public void CalendarControl() {
+		storage = new ServerStorage();
+	}
+	
+	public void addAppointment(Appointment appointment) {
+		storage.insertAppointment(appointment);
+	}
+	public void deleteAppointment(Appointment appointment) {
+		storage.deleteAppointmentById(appointment.getId());
+	}
+	public void updateAppointment(Appointment appointment) {
+		storage.updateAppointment(appointment);
+	}
 
 }
