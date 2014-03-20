a5b889a399a52301150b41cdd777c158b97fe1e7
Lima92 <anders92lima@yahoo.no>
2014-03-20 17:21:49 +0100

    Added Person selector and refresh button.

--- src/no/ntnu/pu/gui/view/MainView.java
+++ src/no/ntnu/pu/gui/view/MainView.java
@@ -1,6 +1,8 @@
 package no.ntnu.pu.gui.view;
 
+import javafx.scene.PerspectiveCamera;
 import no.ntnu.pu.control.CalendarControl;
+import no.ntnu.pu.control.PersonControl;
 import no.ntnu.pu.model.*;
 
 import javax.swing.*;
@@ -12,20 +14,20 @@ import java.util.Date;
 public class MainView {
     private JPanel container, calendarView, notificationView, meetingsView;
 
-    private JButton newAppointmentButton, monthButton, weekButton;
+    private JButton newAppointmentButton, monthButton, weekButton, refreshButton;
     private ButtonGroup buttonGroup;
     private MonthView monthView;
     private WeekView weekView;
+    private JComboBox<Person> dropdown;
 
     public MainView(){
 
         JFrame frame = new JFrame("Gigakalender");
+        frame.setDefaultLookAndFeelDecorated(true);
         frame.setResizable(false);
         container = new JPanel(new GridBagLayout());
 
         GridBagConstraints gbc = new GridBagConstraints();
-        //gbc.insets = new Insets(10,10,10,10);
-        gbc.anchor = GridBagConstraints.LINE_START;
 
         calendarView = new JPanel();
         monthView = new MonthView();
@@ -40,10 +42,19 @@ public class MainView {
         monthButton.setEnabled(false);
         weekButton = new JButton("Uke");
         newAppointmentButton = new JButton("Ny avtale");
+        refreshButton = new JButton("Oppdater");
+
+        dropdown = new JComboBox<Person>();
+
+        for(Person p : PersonControl.getAll()){
+            dropdown.addItem(p);
+        }
 
         monthButton.addActionListener(new ButtonListener());
         weekButton.addActionListener(new ButtonListener());
         newAppointmentButton.addActionListener(new ButtonListener());
+        refreshButton.addActionListener(new ButtonListener());
+
 
         calendarView.add(monthView);
         calendarView.add(weekView);
@@ -53,15 +64,21 @@ public class MainView {
         buttonGroup.add(weekButton);
         buttonGroup.add(monthButton);
 
-
+        container.add(dropdown, GBC(gbc,0,0));
         container.add(notificationView, GBC(gbc, 0, 1));
         container.add(meetingsView, GBC(gbc, 0, 2));
         container.add(newAppointmentButton, GBC(gbc, 1, 0));
-        container.add(weekButton, GBC(gbc, 2, 0));
-        container.add(monthButton, GBC(gbc, 3, 0));
-        GridBagConstraints c = GBC(gbc, 1, 1);
-        c.gridwidth=3;
-        c.gridheight=2;
+        GridBagConstraints c = GBC(gbc, 2, 0);
+        c.anchor = GridBagConstraints.EAST;
+        container.add(weekButton, c);
+        c.gridx = 3;
+        container.add(monthButton, c);
+        c.gridx = 4;
+        container.add(refreshButton, c);
+        c = GBC(gbc, 1, 1);
+        c.anchor = GridBagConstraints.CENTER;
+        c.gridwidth = 4;
+        c.gridheight = 2;
         container.add(calendarView, c);
 
         frame.getContentPane().add(container);
@@ -84,6 +101,7 @@ public class MainView {
     }
 
     private GridBagConstraints GBC (GridBagConstraints c, int x, int y){
+        c.anchor = GridBagConstraints.WEST;
         c.gridx = x;
         c.gridy = y;
         return c;
@@ -104,6 +122,8 @@ public class MainView {
                 showWeek();
                 weekButton.setEnabled(false);
                 monthButton.setEnabled(true);
+            }else if(s.equals(refreshButton)){
+                CalendarControl.refresh();
             }
         }
     }
